833c25a737109d5225e77d8b08f3a15e0a3962b6
Yongjin Kim <span5189@naver.com>
2025-03-14 17:30:20 +0900

    깃 프로젝트 시작

--- src/com/msa2025/Hello.java
+++ src/com/msa2025/Hello.java
@@ -4,17 +4,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
-		System.out.println("Hello world");
-		System.out.println("브라우저 수정 Hello world");
-		System.out.println("로컬에서 수정 Hello world");
-		System.out.println("로컬에서 수정2 Hello world");
-		System.out.println("로컬에서 수정2 Hello world");
-		System.out.println("로컬에서 수정123 Hello world");
-		
-		
-		System.out.println("로컬에서 수정2 Hello world");
-		System.out.println("로컬에서 수정2 Hello world");
+		/*
+		 * msa과제, 프로젝트를 관리하는 깃입니다
+		 */
+		System.out.println("Hello");
 	}
 
 }
